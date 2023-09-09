e739eae66008cdedd2d780c09ffb29ece477dc10
Tanisha Srivastava <138584422+tanishasrivastava@users.noreply.github.com>
2023-09-09 14:45:37 +0530

    Create Sparse Matrix Using Linked List.java

---
+++ Sparse Matrix Using Linked List.java
@@ -0,0 +1,50 @@
+class Node {
+    int row;
+    int col;
+    int value;
+    Node next;
+
+    Node(int r, int c, int val) {
+        row = r;
+        col = c;
+        value = val;
+    }
+}
+
+public class Sparse {
+    public static void main(String[] args) {
+        /* Assume a 4x4 sparse matrix */
+        int sparseMatrix[][] = {
+            {0, 0, 1, 2},
+            {3, 0, 0, 0},
+            {0, 4, 5, 0},
+            {0, 6, 0, 0}
+        };
+
+        Node start = null; /* Start with an empty list */
+        Node tail = null;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (sparseMatrix[i][j] != 0) { /* Pass only non-zero values */
+                    Node temp = new Node(i, j, sparseMatrix[i][j]);
+                    temp.next = null;
+
+                    if (start == null) {
+                        start = temp;
+                        tail = temp;
+                    } else {
+                        tail.next = temp;
+                        tail = tail.next;
+                    }
+                }
+            }
+        }
+
+        Node itr = start;
+        while (start != null) {
+            System.out.println(start.row + " " + start.col + " " + start.value);
+            start = start.next;
+        }
+    }
+}
