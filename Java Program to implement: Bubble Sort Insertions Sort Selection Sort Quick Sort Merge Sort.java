3059f79c8419ccac869ceb70caca0976cbf1ab89
Tanisha Srivastava <138584422+tanishasrivastava@users.noreply.github.com>
2023-11-21 17:29:50 +0530

    Create Java Program to implement: Bubble Sort Insertions Sort Selection Sort Quick Sort Merge Sort.java

---
+++ Java Program to implement: Bubble Sort Insertions Sort Selection Sort Quick Sort Merge Sort.java
@@ -0,0 +1,138 @@
+import java.util.Arrays;
+public class Main {
+    // Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    // Insertion Sort
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    // Selection Sort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap arr[i] and arr[minIndex]
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    // Quick Sort
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int partitionIndex = partition(arr, low, high);
+            quickSort(arr, low, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, high);
+        }
+    }
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        // Swap arr[i + 1] and arr[high] (pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    // Merge Sort
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = Arrays.copyOfRange(arr, left, left + n1);
+        int[] rightArr = Arrays.copyOfRange(arr, mid + 1, mid + 1 + n2);
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+
+        // Bubble Sort
+        bubbleSort(arr.clone());
+        System.out.println("Bubble Sort: " + Arrays.toString(arr));
+
+        // Insertion Sort
+        insertionSort(arr.clone());
+        System.out.println("Insertion Sort: " + Arrays.toString(arr));
+
+        // Selection Sort
+        selectionSort(arr.clone());
+        System.out.println("Selection Sort: " + Arrays.toString(arr));
+
+        // Quick Sort
+        quickSort(arr.clone(), 0, arr.length - 1);
+        System.out.println("Quick Sort: " + Arrays.toString(arr));
+
+        // Merge Sort
+        mergeSort(arr.clone(), 0, arr.length - 1);
+        System.out.println("Merge Sort: " + Arrays.toString(arr));
+    }
+}
